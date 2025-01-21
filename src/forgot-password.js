4bae5ca0336f2a263c877661a7e3860188b1a806
Abhishek Kange <84184526+Abhishekkange@users.noreply.github.com>
2025-01-21 12:25:25 +0530

    url console

--- src/forgot-password.js
+++ src/forgot-password.js
@@ -147,6 +147,7 @@ const getUserDetailsByEmail = async (email,realm_name,server_url,token) => {
     const sendResetPasswordEmail = async (realmName, userId,token,SERVER_URL) => {
         try {
             const url = `https://${SERVER_URL}/auth/realms/admin/realms/${realmName}/users/${userId}/reset-password-email`;
+            console.log("URL:",url);
     
             const response = await axios.put(
                 url,
